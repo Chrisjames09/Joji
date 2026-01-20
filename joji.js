0cfd58f48f3d285a395b7341544112242a12e095
Chrisjames09 <mrchrisjames09@gmail.com>
2026-01-20 12:49:35 +0800

    Add files via upload

---
+++ joji.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('img');
+
+    images.forEach(img => {
+        img.addEventListener('mouseover', function() {
+            img.style.transform = 'scale(1.1)';
+        });
+
+        img.addEventListener('mouseout', function() {
+            img.style.transform = 'scale(1)';
+        });
+    });
+});
+
+
